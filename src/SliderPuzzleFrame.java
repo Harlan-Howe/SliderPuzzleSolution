0c9ac4dc559cffee40a0d0c66ec06e986d95cb03
harlan.howe <harlan.howe@kinkaid.org>
2023-11-28 08:01:02 -0600

    Added reset button. Resized window to fit slider better.

--- src/SliderPuzzleFrame.java
+++ src/SliderPuzzleFrame.java
@@ -1,15 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SliderPuzzleFrame extends JFrame
+public class SliderPuzzleFrame extends JFrame implements ActionListener
 {
     private SliderPuzzlePanel mainPanel;
+    private JButton resetButton;
 
     public SliderPuzzleFrame()
     {
         super("Slider Puzzle");
         mainPanel = new SliderPuzzlePanel();
-        setSize(800,800);
+        setSize(564,600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         buildGUI();
@@ -19,6 +22,14 @@ public class SliderPuzzleFrame extends JFrame
     {
         getContentPane().setLayout(new BorderLayout());
         getContentPane().add(mainPanel,BorderLayout.CENTER);
+        resetButton = new JButton("Reset");
+        getContentPane().add(resetButton,BorderLayout.SOUTH);
+        resetButton.addActionListener(this);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == resetButton)
+            mainPanel.shuffle();
+    }
 }
